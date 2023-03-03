d6c37096bdb2210857138f256736da180f1b89c0
yuliiaishchenko <romashka.ukr@gmail.com>
2023-03-03 12:04:21 +0100

    end

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery') ;
+
+function createGalleryMarkup (items) {
+    return items.map(({preview, original, description}) => {
+        return `<li>
+    <a class = "gallery__item" href = "${original}">
+    <img class = "gallery__image" 
+    src = "${preview}"
+    alt = "${description}"/>
+    </a>
+    </li>`}).join("");
+};
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', addGalleryMarkup);
+
+const lightBox = new SimpleLightbox('.gallery a',{
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
